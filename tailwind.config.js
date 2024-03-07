c7b34d0be174c813c2b841e3c695a4011c4af833
Mehedi Hassan Miraj <dreammehedihassan@gmail.com>
2024-03-07 11:24:48 +0600

    Ready For Working. Basic Structure Ready.

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html, js}"],
+  theme: {
+    container: {
+      center: true,
+      padding: "1.5rem",
+    },
+    fontFamily: {
+      poppins: ["Poppins", "sans-serif"],
+      kanit: ["Kanit", "sans-serif"],
+      nunito: ["Nunito", "sans-serif"],
+    },
+    extend: {
+      colors: {
+        primary: "#2563eb",
+        secondary: "#c026d3",
+        dark: "#020617",
+        border: "#64748b",
+      },
+    },
+  },
+  plugins: [],
+};
